7034602bb90294dd7187411c84842f8868c32726
Elias <elias.westrin@gmail.com>
2026-03-27 10:57:06 +0100

    initial commit

---
+++ src/test/java/edu/vestrin/wigelltravels/WigellTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vestrin.wigelltravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellTravelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
