e47dada2b4cba620e8d72ce23f9eb91f3ead44e3
Elias <elias.westrin@gmail.com>
2026-03-27 15:49:35 +0100

    Fixes on response DTOs. BookingResponse has customerId instead of Customer DTO object.

--- src/main/java/edu/vestrin/wigelltravels/dto/response/BookingResponseDto.java
+++ src/main/java/edu/vestrin/wigelltravels/dto/response/BookingResponseDto.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 public record BookingResponseDto(
         Long id,
-        CustomerResponseDto customer,
+        Long customerId,
         Long destinationId,
         LocalDate departureDate,
         int numOfWeeks,
--- src/main/java/edu/vestrin/wigelltravels/repository/BookingRepository.java
+++ src/main/java/edu/vestrin/wigelltravels/repository/BookingRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByCustomerId(Long customerId);
 }
